5c83f46c4d6a4800abddc9562514f481ee8c4708
pbonifati <paolo.bonifati96@gmail.com>
2023-11-30 17:07:33 +0100

    Prova 1

--- ExtendedKalmanFilter.py
+++ ExtendedKalmanFilter.py
@@ -2,11 +2,11 @@
     Implementation of the Extended Kalman Filter
     for an unactuated pendulum system
 """
-import numpy as np 
+import numpy as np
+
 
 class ExtendedKalmanFilter(object):
-    
-    
+
     def __init__(self, x0, P0, Q, R, dT):
         """
            Initialize EKF
@@ -18,30 +18,26 @@ class ExtendedKalmanFilter(object):
             R  - covariance matrix of the measurement noise
             dT - discretization step for forward dynamics
         """
-        self.x0=x0
-        self.P0=P0
-        self.Q=Q
-        self.R=R
-        self.dT=dT
-        
-        
+        self.x0 = x0
+        self.P0 = P0
+        self.Q = Q
+        self.R = R
+        self.dT = dT
+
         self.g = 9.81  # Gravitational constant
         self.l = 1  # Length of the pendulum 
-        
+
         self.currentTimeStep = 0
-        
 
         self.priorMeans = []
         self.priorMeans.append(None)  # no prediction step for timestep=0
         self.posteriorMeans = []
         self.posteriorMeans.append(x0)
-        
-        self.priorCovariances=[]
+
+        self.priorCovariances = []
         self.priorCovariances.append(None)  # no prediction step for timestep=0
-        self.posteriorCovariances=[]
+        self.posteriorCovariances = []
         self.posteriorCovariances.append(P0)
-    
-    
 
     def stateSpaceModel(self, x, t):
         """
@@ -58,18 +54,17 @@ class ExtendedKalmanFilter(object):
             Returns:
                 f : dx(t)/dt, describes the system of ODEs
         """
-        dxdt = np.array([[x[1,0]], [-(self.g/self.l)*np.sin(x[0,0])]])
+        dxdt = np.array([[x[1, 0]], [-(self.g / self.l) * np.sin(x[0, 0])]])
         return dxdt
-    
+
     def discreteTimeDynamics(self, x_t):
         """
             Forward Euler integration.
             
             returns next state as x_t+1 = x_t + dT * (dx/dt)|_{x_t}
         """
-        x_tp1 = x_t + self.dT*self.stateSpaceModel(x_t, None)
+        x_tp1 = x_t + self.dT * self.stateSpaceModel(x_t, None)
         return x_tp1
-    
 
     def jacobianStateEquation(self, x_t):
         """
@@ -79,15 +74,17 @@ class ExtendedKalmanFilter(object):
             Parameters:
                 x_t : state variables (column-vector)
         """
-        A = np.zeros(shape=(<...>))  # TODO: shape?
-        
+        # A = np.zeros((2,2))  # TODO: shape?
+        # aggiunto
+
         # TODO
         # compute the Jacobian of the discrete dynamics
         # ...
 
+        A = (np.array([[1, self.dT], [-self.dT*(self.g / self.l) * np.cos(x_t[0, 0]), 1]]))  # aggiunto
+
         return A
-    
-    
+
     def jacobianMeasurementEquation(self, x_t):
         """
             Jacobian of measurement model.
@@ -95,38 +92,38 @@ class ExtendedKalmanFilter(object):
             Measurement model is linear, hence its Jacobian
             does not actually depend on x_t
         """
-        C = np.zeros(shape=(<...>))  # TODO: shape?
-        
+        C = np.zeros((1, 2))  # TODO: shape?
+        # aggiunto
+
         # TODO
         # compute the Jacobian of the measurement model
         # ...
 
-        return C
-    
-     
-    def forwardDynamics(self):
-        self.currentTimeStep = self.currentTimeStep+1  # t-1 ---> t
+        C = np.array([[1, 0]])  # aggiunto
 
-        
+        return C
+
+    def forwardDynamics(self):
+        self.currentTimeStep = self.currentTimeStep + 1  # t-1 ---> t
+
         """
             Predict the new prior mean for timestep t
         """
-        x_t_prior_mean = self.discreteTimeDynamics(self.posteriorMeans[self.currentTimeStep-1])
-        
+        x_t_prior_mean = self.discreteTimeDynamics(self.posteriorMeans[self.currentTimeStep - 1])
 
         """
             Predict the new prior covariance for timestep t
         """
         # Linearization: jacobian of the dynamics at the current a posteriori estimate
-        A_t_minus = self.jacobianStateEquation(self.posteriorMeans[self.currentTimeStep-1])
+        A_t_minus = self.jacobianStateEquation(self.posteriorMeans[self.currentTimeStep - 1])
 
         # TODO: propagate the covariance matrix forward in time
-        x_t_prior_cov = # ...
-        
+        x_t_prior_cov = np.dot(A_t_minus, np.dot(self.posteriorCovariances[self.currentTimeStep - 1],
+                                                 A_t_minus.T)) + self.Q  # aggiunto
+
         # Save values
         self.priorMeans.append(x_t_prior_mean)
         self.priorCovariances.append(x_t_prior_cov)
-    
 
     def updateEstimate(self, z_t):
         """
@@ -135,17 +132,20 @@ class ExtendedKalmanFilter(object):
         """
 
         # Jacobian of measurement model at x_t
-        Ct = self.jacobianMeasurementEquation(self.priorMeans[self.currentTimeStep]) 
-        
+        Ct = self.jacobianMeasurementEquation(self.priorMeans[self.currentTimeStep])
+
         # TODO: Compute the Kalman gain matrix
-        K_t = # ...
-        
+        tmp = np.dot(Ct, np.dot(self.priorCovariances[self.currentTimeStep], Ct.transpose())) + self.R
+        K_t = np.dot(self.priorCovariances[self.currentTimeStep], np.dot(Ct.transpose(), np.linalg.inv(tmp)))  # aggiunto
+
         # TODO: Compute posterior mean
-        x_t_mean = # ...
-        
+        x_t_mean = self.priorMeans[self.currentTimeStep] + np.dot(K_t, (
+                    z_t - np.dot(Ct, self.priorMeans[self.currentTimeStep])))  # aggiunto
+
         # TODO: Compute posterior covariance
-        x_t_cov = # ...
-        
+        x_t_cov = np.dot((1 - np.dot(K_t, Ct)), self.priorCovariances[self.currentTimeStep])  # aggiunto
+        #print(x_t_mean)
+
         # Save values
         self.posteriorMeans.append(x_t_mean)
         self.posteriorCovariances.append(x_t_cov)
--- main.py
+++ main.py
@@ -15,43 +15,43 @@ from scipy.integrate import odeint
 from ExtendedKalmanFilter import ExtendedKalmanFilter
 
 # Discretization time step (frequency of measurements)
-deltaTime=0.01
+deltaTime = 0.01
 
 # Initial true state
-x0 = np.array([np.pi/3, 0.5])
+x0 = np.array([np.pi / 3, 0.5])
 
 # Simulation duration in timesteps
-simulationSteps=400
-totalSimulationTimeVector=np.arange(0, simulationSteps*deltaTime, deltaTime)
+simulationSteps = 400
+totalSimulationTimeVector = np.arange(0, simulationSteps * deltaTime, deltaTime)
+
 
 # System dynamics (continuous, non-linear) in state-space representation (https://en.wikipedia.org/wiki/State-space_representation)
-def stateSpaceModel(x,t):
+def stateSpaceModel(x, t):
     """
         Dynamics may be described as a system of first-order
         differential equations: 
         dx(t)/dt = f(t, x(t))
     """
-    g=9.81
-    l=1
-    dxdt=np.array([x[1], -(g/l)*np.sin(x[0])])
+    g = 9.81
+    l = 1
+    dxdt = np.array([x[1], -(g / l) * np.sin(x[0])])
     return dxdt
+
 
 # True solution x(t)
 x_t_true = odeint(stateSpaceModel, x0, totalSimulationTimeVector)
-
-
 
 """
     EKF initialization
 """
 # Initial state belief distribution (EKF assumes Gaussian distributions)
-x_0_mean = np.zeros(shape=(2,1))  # column-vector
-x_0_mean[0] = x0[0] + 3*np.random.randn()
-x_0_mean[1] = x0[1] + 3*np.random.randn()
-x_0_cov = 10*np.eye(2,2)  # initial value of the covariance matrix
+x_0_mean = np.zeros(shape=(2, 1))  # column-vector
+x_0_mean[0] = x0[0] + 3 * np.random.randn()
+x_0_mean[1] = x0[1] + 3 * np.random.randn()
+x_0_cov = 10 * np.eye(2, 2)  # initial value of the covariance matrix
 
 # Process noise covariance matrix (close to zero, we do not want to model noisy dynamics)
-Q=0.00001*np.eye(2,2)
+Q = 0.00001 * np.eye(2, 2)
 
 # Measurement noise covariance matrix for EKF
 R = np.array([[0.05]])
@@ -59,31 +59,45 @@ R = np.array([[0.05]])
 # create the extended Kalman filter object
 EKF = ExtendedKalmanFilter(x_0_mean, x_0_cov, Q, R, deltaTime)
 
-
-
 """
     Simulate process
 """
 measurement_noise_var = 0.05  # Actual measurement noise variance (unknown to the user)
 
-for t in range(simulationSteps-1):
+for t in range(simulationSteps - 1):
     # PREDICT step
     EKF.forwardDynamics()
-    
+
     # Measurement model
-    z_t = x_t_true[t, 0] + np.sqrt(measurement_noise_var)*np.random.randn()
+    z_t = x_t_true[t, 0] + np.sqrt(measurement_noise_var) * np.random.randn()
 
     # UPDATE step
     EKF.updateEstimate(z_t)
 
-
-
 """
     Plot the true vs. estimated state variables
 """
+
+
 ### Estimates
 # EKF.posteriorMeans
 # EKF.posteriorCovariances
 
 # TODO
 # ...
+
+def Extract(lst):
+    return np.array(list(list(zip(*lst))[0]))
+
+
+tmp = np.array([[1], [0], [2]])
+plt.plot(tmp)
+# plt.plot(Extract(EKF.posteriorMeans))
+# print(np.array(EKF.posteriorMeans))
+# print(x_t_true)
+
+
+raws = [(np.random.rand(20, 100), np.random.rand(20, 100))]
+f, axes = plt.subplots(len(raws[0]), 1)
+for i in range(len(raws[0])):
+    axes[i].plot(raws[0][i])
